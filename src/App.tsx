77a379cfcf6896bb5d960f2c45e5417773a17d96
Cicero Wesley <wesleycariutaba@gmail.com>
2024-08-07 16:20:45 -0300

    setup react

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.scss'
+
+
+function App() {
+
+  return (
+    <>
+      <p className='classe1'>Teste</p>
+      
+    </>
+  )
+}
+
+export default App
